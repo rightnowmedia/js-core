daf5ab21e8907b03e1bb40c4e9ae336b84258731
rightnowmedia <102834106+rightnowmedia@users.noreply.github.com>
2025-09-10 13:27:50 -0500

    Update and rename rightnowpastors.2.js to rightnowpastors.3.js

--- rightnowpastors.2.js
+++
@@ -1,3 +0,0 @@
-import { setupPopups } from 'https://cdn.jsdelivr.net/gh/rightnowmedia/js-core/core/popups.2.js';
-
-setupPopups();
---
+++ rightnowpastors.3.js
@@ -0,0 +1,3 @@
+import { setupPopups } from 'https://cdn.jsdelivr.net/gh/rightnowmedia/js-core/core/popups.3.js';
+
+setupPopups();
